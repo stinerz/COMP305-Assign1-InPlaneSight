8d15e2e7ed6c54e945c9a7380940562c5902a9c1
Christina Kuo <christinakuo@Christinas-MacBook-Pro.local>
2016-09-27 23:51:03 -0400

    new assets and player movement

--- Assets/_Scripts/BirdController.cs
+++ Assets/_Scripts/BirdController.cs
@@ -2,14 +2,60 @@
 using System.Collections;
 
 public class BirdController : MonoBehaviour {
+	//Private Instance Variables 
+	private int _speed; 
+	private Transform _transform; 
+	private Vector2 _position; 
+
+	public int Speed{
+		get{
+			return this._speed;
+		}
+
+		set{ 
+			this._speed = value; 
+		}
+
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		//transform component from the object, getting ref and storing in private class 
+		this._transform = this.GetComponent<Transform> (); 
+
+		//sets the speed 
+		this._speed = 4; 
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update (){
+		this._move (); 
+		this._checkBoundary ();
+	}
+
+	void _move () {
+		//ref Vector2
+		this._position = this._transform.position; 
+
+		//add speed to position, moving to the left  
+		this._position.x -= this._speed; 
+
+		//assign position to new position
+		this._transform.position = this._position; 
+	}
+
+	//Method check game object meets right-border
+	void _checkBoundary(){
+		if (this._transform.position.x <= -350f) {
+			this._reset (); 
+		}
+	}
+
+	//Method resets the game object to original position
+	void _reset(){
+		//this will shift the object back
+		//randomly place coins on background 
+		this._transform.position = new Vector2 (350f, Random.Range(-190f, 190f)); 
 	}
 }
---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+	// Movement modifier applied to directional movement.
+	public float playerSpeed = 10.0f;
+	// What the current speed of our player is
+	private float currentSpeed = 0.0f;
+	// The last movement that we've made
+	private Vector3 lastMovement = new Vector3();
+
+
+	public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Move the player's body
+		Movement();
+
+	}
+		
+	// Will move the player based off of keys pressed
+	void Movement()
+	{
+		// The movement that needs to occur this frame
+		Vector3 movement = new Vector3();
+		// Check for input
+		movement.x += Input.GetAxis ("Horizontal");
+		movement.y += Input.GetAxis ("Vertical");
+		/*
+		* If we pressed multiple buttons, make sure we're only
+		* moving the same length.
+		*/
+		movement.Normalize ();
+		// Check if we pressed anything
+		if(movement.magnitude > 0)
+		{
+			// If we did, move in that direction
+			currentSpeed = playerSpeed;
+			this.transform.Translate(movement * Time.deltaTime * playerSpeed, Space.World);
+			lastMovement = movement;
+
+			mainCamera.transform.Translate (movement * Time.deltaTime * playerSpeed, Space.World);
+		}
+		else
+		{
+			// Otherwise, move in the direction we were going
+			this.transform.Translate(lastMovement * Time.deltaTime *
+				currentSpeed, Space.World);
+
+			mainCamera.transform.Translate(lastMovement * Time.deltaTime *currentSpeed, Space.World);
+
+			// Slow down over time
+			//currentSpeed *= 3f;
+		}
+	}
+
+}
